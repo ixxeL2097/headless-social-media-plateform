678b9f9a707db0bd14eddadfe862d57d4e2ea87e
Lucas Tchitdjian <tchitdjian.lucas@gmail.com>
2024-11-24 15:29:41 +0100

    add Home component for the main landing page

---
+++ social-media-website/src/components/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Social Media Platform</h1>
+            <p>This is the home page of our social media website.</p>
+        </div>
+    );
+};
+
+export default Home;
