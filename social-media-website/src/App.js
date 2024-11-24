d1b5d490d62b6be52850149e243365f18cbe13c4
Lucas Tchitdjian <tchitdjian.lucas@gmail.com>
2024-11-24 15:28:56 +0100

    enhance routing and styling: integrate react-router for navigation and update CSS for improved layout

--- social-media-website/src/App.js
+++ social-media-website/src/App.js
@@ -1,10 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import CreatePost from './components/Pages/Posts/CreatePost';
+import Home from './components/Pages/Home/Home';
+import Register from './components/Pages/Register/Register';
+import Login from './components/Pages/Login/Login';
+import Profile from './components/Pages/Profile/Profile';
+import MyPosts from './components/Pages/MyPosts/MyPosts';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/content/create" element={<CreatePost />} />
+        <Route path="/content/my-posts" element={<MyPosts />} />
+        <Route path='/auth/register' element={<Register />} />
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/profile/:userId' element={<Profile />} />
+      </Routes>
     </div>
   );
 }
