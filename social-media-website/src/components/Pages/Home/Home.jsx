27986e0e10cd9a1873e8e1a032f40dd371e7c358
Lucas Tchitdjian <tchitdjian.lucas@gmail.com>
2024-11-25 16:06:56 +0100

    style: add Home component styling for layout and background

--- social-media-website/src/components/Pages/Home/Home.jsx
+++ social-media-website/src/components/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-page'>
             <h1>Welcome to the Social Media Platform</h1>
             <p>This is the home page of our social media website.</p>
         </div>
