c8e404ae61e78d2e3c89dff1c4f978a97547e145
Lucas Tchitdjian <tchitdjian.lucas@gmail.com>
2024-11-25 16:05:18 +0100

    add routing for Explore and MyMedia pages, and wrap app in PostsProvider

--- social-media-website/src/App.js
+++ social-media-website/src/App.js
@@ -7,19 +7,29 @@ import Register from './components/Pages/Register/Register';
 import Login from './components/Pages/Login/Login';
 import Profile from './components/Pages/Profile/Profile';
 import MyPosts from './components/Pages/MyPosts/MyPosts';
+import ExplorePage from './components/Pages/Explore/ExplorePage';
+import MyMediaPage from './components/Pages/MyMedia/MyMediaPage';
+import SinglePost from './components/Pages/SinglePost/SinglePost';
+import { PostsProvider } from './context/PostsContext';
 
 function App() {
+
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/content/create" element={<CreatePost />} />
-        <Route path="/content/my-posts" element={<MyPosts />} />
-        <Route path='/auth/register' element={<Register />} />
-        <Route path='/auth/login' element={<Login />} />
-        <Route path='/profile/:userId' element={<Profile />} />
-      </Routes>
+      <PostsProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/content/create" element={<CreatePost />} />
+          <Route path="/content/my-posts" element={<MyPosts />} />
+          <Route path="/content/post/:id" element={<SinglePost />} />
+          <Route path="/content/explore" element={<ExplorePage />} />
+          <Route path="/media/my-media" element={<MyMediaPage />} />
+          <Route path='/auth/register' element={<Register />} />
+          <Route path='/auth/login' element={<Login />} />
+          <Route path='/profile/:userId' element={<Profile />} />
+        </Routes>
+      </PostsProvider>
     </div>
   );
 }
